f7d04c63df5324f32b77e7e2324f7fb2fcee8af2
MattBlack85 <promat85@gmail.com>
2015-03-28 22:06:23 +0100

    Make CursorLoggingWrapper immediately available

---
+++ query_logger/__init__.py
@@ -0,0 +1,7 @@
+from django.db.backends import utils
+
+from .wrapper import CursorLoggingWrapper
+
+# Monkeypatching django CursorWrapper with our own since there is
+# no way to use a custom one right now.
+utils.CursorWrapper = CursorLoggingWrapper
