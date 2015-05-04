33617e30c6c3138d6efcdc9caa8f01b52ec45223
MattBlack85 <promat85@gmail.com>
2015-05-04 14:38:47 +0200

    Add first version of setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='querylogger',
+    author="Mattia Procopio",
+    author_email="promat85@gmail.com",
+    version='0.1.0',
+    description="Utility for logging queries while running on `DEBUG = False`",
+    license='BSD License',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
