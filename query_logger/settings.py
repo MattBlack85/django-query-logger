480842f3c487cab5b7a45f5be7a2bd2b7d684f84
MattBlack85 <promat85@gmail.com>
2015-05-04 14:23:39 +0200

    Add settings

---
+++ query_logger/settings.py
@@ -0,0 +1,33 @@
+QUERY_LOGGING = {
+    'formatters': {
+        'wrapper_verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+        'wrapper_simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+        'wrapper_json': {
+            '()': 'jsonlogger.JsonFormatter',
+            'format': '%(levelname)s %(asctime)s %(name)s %(module)s %(lineno)d'
+            '%(process)d %(thread)d %(message)s'
+        },
+        'wrapper_simple_json': {
+            '()': 'jsonlogger.JsonFormatter',
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'console_wrapper': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'wrapper_verbose',
+        }
+    },
+    'loggers': {
+        'wrapper': {
+            'handlers': 'console_wrapper',
+            'level': 'INFO',
+            'propagate': True
+        }
+    }
+}
