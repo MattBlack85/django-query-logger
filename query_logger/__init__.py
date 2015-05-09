815377e8e5d2f26811bc25c159aac2e9101b2900
Mattia Procopio <promat85@gmail.com>
2015-05-09 23:10:50 +0200

    Monkeypatch CursorWrapper only on DEBUG = False
    There is no sense monkeypatching the cursor wrapper when debug
    is ON cause another wrapper is used in that situation

--- query_logger/__init__.py
+++ query_logger/__init__.py
@@ -1,7 +1,9 @@
-from django.db.backends import utils
+from django.conf import settings
 
-from .wrapper import CursorLoggingWrapper
+if settings.DEBUG is False:
+    from django.db.backends import utils
+    from .wrapper import CursorLoggingWrapper
 
-# Monkeypatching django CursorWrapper with our own since there is
-# no way to use a custom one right now.
-utils.CursorWrapper = CursorLoggingWrapper
+    # Monkeypatching django CursorWrapper with our own since there is
+    # no way to use a custom one right now.
+    utils.CursorWrapper = CursorLoggingWrapper
