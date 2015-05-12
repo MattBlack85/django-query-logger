db2bf6e1835eb0c8dc6fd91fefee5b6eb2009b84
Mattia Procopio <promat85@gmail.com>
2015-05-12 22:49:41 +0200

    Add the possibility to use custom wrapper on debug = true

--- query_logger/__init__.py
+++ query_logger/__init__.py
@@ -1,9 +1,19 @@
 from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
+from django.db.backends import utils
 
-if settings.DEBUG is False:
-    from django.db.backends import utils
-    from .wrapper import CursorLoggingWrapper
+from .wrapper import CursorLoggingWrapper
 
+QUERY_LOGGER_ON_DEBUG = getattr(settings, 'QUERY_LOGGER_ON_DEBUG', False)
+DEBUG = settings.DEBUG
+
+if QUERY_LOGGER_ON_DEBUG and not DEBUG:
+    raise ImproperlyConfigured(
+        "You can't set QUERY_LOGGER_ON_DEBUG to True while DEBUG is set to False")
+
+if not DEBUG:
     # Monkeypatching django CursorWrapper with our own since there is
     # no way to use a custom one right now.
     utils.CursorWrapper = CursorLoggingWrapper
+elif QUERY_LOGGER_ON_DEBUG:
+    utils.CursorDebugWrapper = CursorLoggingWrapper
--- tests/test_initialize_wrapper.py
+++ tests/test_initialize_wrapper.py
@@ -1,13 +1,42 @@
+import importlib
+import sys
+
+from django.core.exceptions import ImproperlyConfigured
+from django.db.backends.utils import CursorDebugWrapper
 from django.test import TestCase
 from query_logger.wrapper import CursorLoggingWrapper
 
 
 class InitializeWrapperTestCase(TestCase):
 
-    def test_debug_off_wrapper_on(self):
+    def test_debug_false_wrapper_active(self):
         """
         Tests are run by default with DEBUG = False
         so assert the monkeypatching has worked.
         """
         from django.db.backends import utils
         self.assertEqual(utils.CursorWrapper, CursorLoggingWrapper)
+
+    def test_debug_false_query_logger_on_debug_true(self):
+        with self.settings(DEBUG=False,
+                           QUERY_LOGGER_ON_DEBUG=True):
+            with self.assertRaises(ImproperlyConfigured):
+                importlib.reload(sys.modules['query_logger'])
+
+    def test_debug_true_query_logger_on_debug_false(self):
+        with self.settings(DEBUG=True):
+            from django.conf import settings
+            print(settings.__dict__)
+            importlib.reload(sys.modules['query_logger'])
+            from django.db.backends import utils
+            self.assertEqual(utils.CursorDebugWrapper, CursorDebugWrapper)
+
+    def test_debug_true_query_logger_on_debug_true(self):
+        with self.settings(DEBUG=True,
+                           QUERY_LOGGER_ON_DEBUG=True):
+            importlib.reload(sys.modules['query_logger'])
+            from django.db.backends import utils
+            self.assertEqual(utils.CursorDebugWrapper, CursorLoggingWrapper)
+            # Setting back the CursorDebugWrapper since we're playing with global settings which are
+            # not reset between tests
+            utils.CursorDebugWrapper = CursorDebugWrapper
